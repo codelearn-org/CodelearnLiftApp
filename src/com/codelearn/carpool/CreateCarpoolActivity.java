2628786c7108487de8d14a029e26d6b2daf7aea6
Miroojin Bakshi <gaglu231094@gmail.com>
2014-06-13 11:56:45 +0530

    Define views in CreateCarpoolActivity.

--- src/com/codelearn/carpool/CreateCarpoolActivity.java
+++ src/com/codelearn/carpool/CreateCarpoolActivity.java
@@ -3,13 +3,33 @@ package com.codelearn.carpool;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class CreateCarpoolActivity extends Activity {
-	
+	EditText _phoneNo;
+	EditText _location;
+	TimePicker _stime;
+	TimePicker _etime;
+	Button _submit;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_carpool);
+		_phoneNo = (EditText) findViewById(R.id.fld_phn);
+		_location = (EditText) findViewById(R.id.fld_location);
+		_stime = (TimePicker) findViewById(R.id.fld_stime);
+		_etime = (TimePicker) findViewById(R.id.fld_etime);
+		_submit = (Button) findViewById(R.id.carpool_submit);
+		_submit.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				
+			}});
 	}
 
 	@Override
